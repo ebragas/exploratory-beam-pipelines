b497b7f748b19c02942bed0ca2a876eec1e4c868
Eric Bragas <ericbragas412@gmail.com>
2021-02-17 15:53:22 -0800

    rename

--- stream-to-pubsub/pipe.py
+++ stream-to-pubsub/pipe.py
@@ -7,12 +7,7 @@ PROJECT_ID = "beam-udemy"
 TOPIC_ID = "beam-example-input"
 SUBSCRIPTION_ID = "beam-example-input-sub1"
 
-service_account_path = '/Users/ericbragas/dev/exploratory-beam-pipelines/credentials/durable-return-304504-7bda905b0edc.json'
-print("Service account file : ", service_account_path)
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = service_account_path
-
-pipeline_options = PipelineOptions(streaming=True, save_main_session=True)
-# pipeline_options.view_as(StandardOptions).streaming = True
+pipeline_options = PipelineOptions(streaming=True)
 
 p = beam.Pipeline(options=pipeline_options)
 
